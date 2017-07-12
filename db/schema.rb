14586cce4aecf08397beabdf4ae2c75c4f9b05ad
Sai Ch <chsai.btech@gmail.com>
2017-07-12 15:47:06 +0530

    gem lock added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304045434) do
+ActiveRecord::Schema.define(version: 20170711184917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170304045434) do
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
